bba7ec54c9dc6237d58f10bc0347f486359b904f
Tiago Kenji <119114414+tiago-ken@users.noreply.github.com>
2023-02-07 23:57:27 -0300

    Comandos para Alterar a Cor do Robotron

---
+++ main.js
@@ -0,0 +1,65 @@
+const controle = document.querySelectorAll("[data-controle]");
+const estatisticas = document.querySelectorAll("[data-estatistica]");
+
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
+
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos":{
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas":{
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes":{
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
+}
+
+controle.forEach((elemento) => {
+    elemento.addEventListener("click", (evento) => {
+        manipulaDados(evento.target.dataset.controle, evento.target.parentNode);
+        atualizaEstatisticas(evento.target.dataset.peca);
+    })
+})
+
+function manipulaDados(opercao, controle) {
+    const peca = controle.querySelector("[data-contador]");
+
+    if (opercao === "-") {
+        peca.value = parseInt(peca.value) - 1;
+    } else {
+        peca.value = parseInt(peca.value) + 1;
+    }
+}
+
+function atualizaEstatisticas (peca) {
+
+    estatisticas.forEach( (elemento) => {
+        elemento.textContent = parseInt(elemento.textContent) + pecas[peca][elemento.dataset.estatistica];
+    })
+    
+}
+
+function trocaCor (cor) {
+    document.querySelector(".robo").src="img/Robotron " + cor + "/Robotron " + cor + ".png"
+}
